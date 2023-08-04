02bac4c4508d9d0593e4d59d873f480c846c6bd3
MANOJ-M07 <manojmoorthy01@gmail.com>
2023-08-04 16:01:26 +0530

    initial commit

---
+++ ThreadingExample2/ThreadingExample2/Program.cs
@@ -0,0 +1,73 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingExample2
+{
+    internal class Program
+    {
+        private static object lockObject;
+
+        public static class Counter
+        {
+            public static int CounterValue { get; set; }
+        }
+
+        public static void IncrementCounter()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                lock (lockObject)
+                {
+                    Counter.CounterValue++;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            lockObject = new object();
+            Thread thread1 = new Thread(IncrementCounter);
+            Thread thread2 = new Thread(IncrementCounter);
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+            Console.WriteLine("final Counter Value"+Counter.CounterValue);
+            Console.ReadKey();
+        }
+    }
+}*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadingExample2
+{
+    internal class Program
+    {
+        private static void PrintNumbers(int num)
+        {
+            for (int i = 0; i <= num; i++)
+            {
+                Console.WriteLine($"thread{Thread.CurrentThread.ManagedThreadId} : {i}");
+                Thread.Sleep(1000);
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Thread thread1 = new Thread(() => PrintNumbers(10));
+            Thread thread2 = new Thread(() => PrintNumbers(12));
+            thread1.Start();
+            thread2.Start();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ThreadingOne/ThreadingOne/Program.cs
@@ -0,0 +1,104 @@
+﻿/*using System;
+namespace ThreadingOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to thread");
+            Console.ReadKey();
+        }
+    }
+}
+*/
+/*using System;
+using System.Threading;
+
+namespace ThreadingOne
+{
+    internal class Program
+    {
+        public static void MethodOne()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("MEthod One:");
+                Console.Write(i + "\t");
+                Thread.Sleep(500);
+            }
+        }
+        public static void MethodTwo()
+        {
+            for (int i =20; i < 30; i++)
+            {
+                Console.WriteLine("MEthod Two:");
+                Console.Write(i + "\t");
+                Thread.Sleep(500);
+            }
+        }
+        public static void MethodThree()
+        {
+            for (int i = 40; i < 50; i++)
+            {
+                Console.WriteLine("MEthod Three:");
+                Console.Write(i + "\t");
+                Thread.Sleep(500);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to thread");
+            MethodOne();
+            MethodTwo();
+            MethodThree();
+            Console.ReadKey();
+        }
+    }
+}*/
+using System;
+using System.Threading;
+
+namespace ThreadingOne
+{
+    internal class Program
+    {
+        public static void MethodOne()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("MEthod One:");
+                Console.Write(i + "\t");
+                Thread.Sleep(500);
+            }
+        }
+        public static void MethodTwo()
+        {
+            for (int i = 20; i < 30; i++)
+            {
+                Console.WriteLine("MEthod Two:");
+                Console.Write(i + "\t");
+                Thread.Sleep(500);
+            }
+        }
+        public static void MethodThree()
+        {
+            for (int i = 40; i < 50; i++)
+            {
+                Console.WriteLine("MEthod Three:");
+                Console.Write(i + "\t");
+                Thread.Sleep(500);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to thread");
+            Thread thread1 = new Thread(MethodOne);
+            Thread thread2 = new Thread(MethodTwo);
+            Thread thread3 = new Thread(MethodThree);
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+            Console.ReadKey();
+        }
+    }
+}
